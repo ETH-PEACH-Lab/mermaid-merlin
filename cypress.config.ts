7490d4ac51e31c91d733339a70946c08433c964f
Your Name <xbotraid@gmail.com>
2025-07-04 00:46:01 +0200

    fix: Fourth time trying to fix CI/CD

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'cypress';
 import coverage from '@cypress/code-coverage/task';
 import eyesPlugin from '@applitools/eyes-cypress';
 
+// Load cypress-image-snapshot plugin via wrapper to avoid ES module issues in CI
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
+const { addMatchImageSnapshotPlugin } = require('./cypress-image-snapshot-loader.js');
 
 export default eyesPlugin(
   defineConfig({
